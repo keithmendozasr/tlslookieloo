3d17e796732a6c871e1ea8b7a69c934c73bf0d1e
Keith Mendoza <keith@homepluspower.info>
2019-05-27 18:14:08 -0700

    TCP socket base wrapper

---
+++ include/socketinfo.h
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2019-present tlslookieloo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <stdexcept>
+#include <system_error>
+#include <string>
+#include <memory>
+
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include <log4cplus/logger.h>
+
+namespace tlslookieloo
+{
+
+/**
+ * Class to manage sockets
+ */
+class SocketInfo
+{
+public:
+    /**
+     * Default constructor
+     */
+    explicit SocketInfo();
+
+    virtual ~SocketInfo()
+    {
+        closeSocket();
+    }
+
+    /**
+     * Return the socket descriptor
+     */
+    inline const int &getSocket() const
+    {
+        if(sockfd == -1)
+            throw std::logic_error("Socket not created");
+
+        return sockfd;
+    }
+
+    /**
+     * Close the socket connection
+     */
+    inline void closeSocket()
+    {
+        if(sockfd != -1)
+        {
+            shutdown(sockfd, SHUT_RDWR);
+            close(sockfd);
+            sockfd = -1;
+        }
+    }
+
+    /**
+     * Get the IP address the socket is connected to
+     */
+    const std::string getSocketIP() const;
+    
+    /**
+     * Access the sockaddr_storage structure associated with the socket connection
+     */
+    inline const struct sockaddr_storage &getAddrInfo() const
+    {
+        return addrInfo;
+    }
+
+    /**
+     * Get the address info structure's size
+     */
+    inline const size_t &getAddrInfoSize() const
+    {
+        return addrInfoSize;
+    }
+
+    /**
+     * Wait for data to be available for reading
+     *
+     * \throws std::system_error Error encountered while waiting for message to
+     *  arrive
+     * \throws TimeoutException when timeout reached while waiting for data to
+     *  arrive
+     * \param timeout Number of seconds to wait for data. 0 for no timeout
+     */
+    void waitForReading(const unsigned int timeout = 0);
+
+    /**
+     * Attempt to read from client.
+     *
+     * \throws std::invalid_argument data parameter is null
+     * \param data Buffer to place received data
+     * \param dataSize Size of data
+     * \return Number of bytes received
+     */
+    const size_t readData(char *data, const size_t &dataSize);
+
+    /**
+     * Wait for socket to be ready for writing
+     *
+     * \throws std::system_error Error encountered while waiting for socket to
+     *  be ready to send message
+     * \throws TimeoutException when timeout reached while waiting for socket
+     *  to be ready to send message
+     * \param timeout Number of seconds to wait for socket to be writable. 0
+     *  for no timeout
+     */
+    void waitForWriting(const unsigned int timeout = 0);
+
+    /**
+     * Attempt to send to client
+     *
+     * \param msg Data to send
+     * \param msgSize Size of msg
+     * \return Number of bytes sent
+     */
+    const size_t writeData(const char *msg, const size_t &msgSize);
+
+protected:
+    /**
+     * Resolve the host and port in preparation for connection
+     *
+     * \throws invalid_argument if port param not valid port number
+     * \throws logic_error is socket instance already created
+     * \throws runtime_error if unable to create socket instance
+     * \param port Port number to use with socket connection
+     * \param host Host to connect to as client, or host to listen from as
+     *  server
+     */
+    void resolveHostPort(const unsigned int &port, const std::string &host = "");
+
+    /**
+     * Initialize a socket connection for the next IP
+     *
+     * \throws range_error No more available IP's to try
+     * \throws logic_error host/port resolution has not been completed yet
+     * \throws runtime_error if unable to create socket instance
+     */
+     void initNextSocket();
+
+    /**
+     * Set the sockaddr_storage structure to associate to this class' instance
+     *
+     * \param addr Pointer to sockaddr_storage to asssociate
+     */
+    void setAddrInfo(const sockaddr_storage *addr, const size_t &addrSize);
+
+    inline void throwSystemError(const int &err, const std::string &msg = "")
+    {
+        if(msg == "")
+            throw std::system_error(err, std::generic_category());
+        else
+            throw std::system_error(err, std::generic_category(), msg);
+    }
+
+private:
+    log4cplus::Logger logger;
+    struct sockaddr_storage addrInfo;
+    size_t addrInfoSize;
+    int sockfd;
+
+    std::unique_ptr<struct addrinfo, decltype(&freeaddrinfo)> servInfo;
+    struct addrinfo *nextServ = nullptr;
+
+    timeval timeout;
+};
+
+} //namespace tlslookieloo
---
+++ src/socketinfo.cpp
@@ -0,0 +1,287 @@
+/*
+ * Copyright 2019-present tlslookieloo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <cstring>
+#include <system_error>
+#include <memory>
+
+#include <log4cplus/loggingmacros.h>
+
+#include "socketinfo.h"
+#include "timeoutexception.h"
+
+using namespace std;
+using namespace log4cplus;
+
+namespace tlslookieloo
+{
+
+SocketInfo::SocketInfo() :
+    logger(Logger::getInstance("SocketInfo")),
+    sockfd(-1),
+    servInfo(nullptr, &freeaddrinfo)
+{}
+
+void SocketInfo::resolveHostPort(const unsigned int &port, const string &host)
+{
+    if(sockfd != -1)
+    {
+        if(servInfo)
+            throw logic_error("Instance already initialized");
+    }
+
+    if(port > 65535)
+        throw invalid_argument("port parameter > 65535");
+
+    int rv;
+    struct addrinfo hints;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    LOG4CPLUS_TRACE(logger, "Getting address info");
+    struct addrinfo *tmp;
+    rv = getaddrinfo(
+        (host.size() ? host.c_str() : nullptr),
+        to_string(port).c_str(),
+        &hints, &tmp);
+    if (rv != 0)
+    {
+        throw runtime_error(gai_strerror(rv));
+    }
+
+    servInfo = unique_ptr<struct addrinfo, decltype(&freeaddrinfo)>(
+        tmp, &freeaddrinfo);
+    LOG4CPLUS_TRACE(logger, "servInfo set: " <<
+        (servInfo ? "yes" : "no"));
+    nextServ = servInfo.get();
+}
+
+void SocketInfo::initNextSocket()
+{
+    LOG4CPLUS_TRACE(logger,
+        "Value of servInfo at start: " << servInfo.get() <<
+        " nextServ: " << nextServ
+    );
+    if(!servInfo)
+        throw logic_error("Host info not resolved yet");
+    else if(nextServ == nullptr)
+        throw range_error("No more resolved IP's to try");
+    else
+        LOG4CPLUS_TRACE(logger, "Initialize socket");
+
+    struct addrinfo *addrInfoItem = nextServ;
+    static const bool logTrace = logger.isEnabledFor(TRACE_LOG_LEVEL);
+    if(logTrace)
+    {
+        char hostname[NI_MAXHOST];
+        int errnum = getnameinfo(addrInfoItem->ai_addr, addrInfoItem->ai_addrlen,
+            &hostname[0], sizeof(hostname), nullptr, 0, NI_NUMERICHOST);
+        if((errnum != 0))
+            LOG4CPLUS_TRACE(logger, "getnameinfo errored out: " <<
+                gai_strerror(errnum));
+        else
+            LOG4CPLUS_TRACE(logger, "IP to try: "<<hostname);
+    }
+    LOG4CPLUS_TRACE(logger, "Attempt to get socket");
+    sockfd = socket(addrInfoItem->ai_family,
+        addrInfoItem->ai_socktype | SOCK_NONBLOCK, addrInfoItem->ai_protocol);
+    if (sockfd == -1)
+    {
+        int err = errno;
+        char buf[256];
+        char *errmsg = strerror_r(err, &buf[0], 256);
+        LOG4CPLUS_TRACE(logger, "Value of err: " << err << " String: " <<
+            errmsg);
+        throw runtime_error(string("Failed to get socket fd: ") + errmsg);
+    }
+    else
+    {
+        LOG4CPLUS_DEBUG(logger, "Socket ready");
+        setAddrInfo((const struct sockaddr_storage *)addrInfoItem->ai_addr,
+            addrInfoItem->ai_addrlen);
+        nextServ = addrInfoItem->ai_next;
+    }
+    LOG4CPLUS_TRACE(logger, "Value of nextServ: "<<nextServ);
+}
+
+const string SocketInfo::getSocketIP() const
+{
+    if(sockfd == -1)
+        throw logic_error("Address info not initialized");
+
+    string ip;
+    unique_ptr<char[]>buf;
+    int af;
+    int bufSize;
+    void *addrPtr = nullptr;
+
+    if(addrInfo.ss_family == AF_INET)
+    {
+        LOG4CPLUS_TRACE(logger, __PRETTY_FUNCTION__ << " ai_family is AF_INET");
+        bufSize = INET_ADDRSTRLEN;
+        const struct sockaddr_in *t =
+            reinterpret_cast<const struct sockaddr_in *>(&addrInfo); // NOLINT
+        addrPtr = const_cast<void *>(
+            reinterpret_cast<const void *>(&(t->sin_addr)));
+        af = t->sin_family;
+    }
+    else if(addrInfo.ss_family == AF_INET6)
+    {
+        LOG4CPLUS_TRACE(logger, __PRETTY_FUNCTION__ << " ai_family is AF_INET6");
+        bufSize = INET6_ADDRSTRLEN;
+        const struct sockaddr_in6 *t =
+            reinterpret_cast<const struct sockaddr_in6 *>(&addrInfo); // NOLINT
+        addrPtr = const_cast<void *>(
+            reinterpret_cast<const void *>(&(t->sin6_addr)));
+        af = t->sin6_family;
+    }
+    else
+        throw invalid_argument("Unexpected sa_family value");
+
+    buf = make_unique<char[]>(bufSize);
+    if(inet_ntop(af, addrPtr, buf.get(), bufSize)
+        == nullptr)
+    {
+        auto err = errno;
+        const int errbuflen = 256;
+        char errbuf[errbuflen];
+        char *errmsg = strerror_r(err, &errbuf[0], errbuflen);
+        string msg("Failed to translate IP address: ");
+        msg += errmsg;
+        throw msg;
+    }
+
+    ip = string(buf.get(), bufSize);
+    LOG4CPLUS_TRACE(logger, __PRETTY_FUNCTION__<<" Value of ip: "<<ip);
+    return ip;
+}
+
+void SocketInfo::setAddrInfo(const sockaddr_storage *addr, const size_t &addrSize)
+{
+    memmove(&addrInfo, addr, addrSize);
+    addrInfoSize = addrSize;
+}
+
+void SocketInfo::waitForReading(const unsigned int timeout)
+{
+    fd_set readFd;
+    FD_ZERO(&readFd);
+    FD_SET(sockfd, &readFd); // NOLINT
+
+    timeval waitTime;
+    timeval *timevalPtr = nullptr;
+
+    if(timeout == 0)
+    {
+        waitTime.tv_sec=timeout;
+        waitTime.tv_usec=0;
+        timevalPtr = &waitTime;
+    }
+
+    do
+    {
+        auto retVal = select(sockfd+1, &readFd, nullptr, nullptr, timevalPtr);
+        if(retVal == -1)
+        {
+            auto err = errno;
+            throwSystemError(err,
+                "Error waiting for socket to be ready for reading.");
+        }
+        else if(retVal == 0)
+        {
+            throw TimeoutException("Timeout waiting for data from client");
+        }
+        else if(FD_ISSET(sockfd, &readFd)) // NOLINT
+        {
+            LOG4CPLUS_DEBUG(logger, "Socket ready for reading");
+            break;
+        }
+    } while(true);
+}
+
+const size_t SocketInfo::readData(char *data, const size_t &dataSize)
+{
+    if(data == nullptr)
+        throw invalid_argument("\"data\" parameter is null");
+
+    auto retVal = read(sockfd, data, dataSize);
+    if(retVal < 0)
+    {
+        auto err = errno;
+        throwSystemError(err, "Error reading from socket");
+    }
+    else if(retVal == 0)
+        throwSystemError(EPIPE);
+    else
+        LOG4CPLUS_TRACE(logger, "read complete. Value of retVal: " <<retVal);
+
+    return retVal;
+}
+
+void SocketInfo::waitForWriting(const unsigned int timeout)
+{
+    fd_set writeFd;
+    FD_ZERO(&writeFd);
+    FD_SET(sockfd, &writeFd);
+
+    timeval waitTime;
+    timeval *timevalPtr = nullptr;
+
+    if(timeout == 0)
+    {
+        waitTime.tv_sec = timeout;
+        waitTime.tv_usec = 0;
+        timevalPtr = &waitTime;
+    }
+
+    do
+    {
+        auto retVal = select(sockfd+1, nullptr, &writeFd, nullptr, timevalPtr);
+        if(retVal == -1)
+        {
+            auto err = errno;
+            throwSystemError(err,
+                "Error waiting for socket to be ready for writing.");
+        }
+        else if(retVal == 0)
+        {
+            throw TimeoutException("Timeout waiting for socket to be ready");
+        }
+        else if(FD_ISSET(sockfd, &writeFd))
+        {
+            LOG4CPLUS_DEBUG(logger, "Socket ready for writing");
+            break;
+        }
+    } while(1);
+}
+
+const size_t SocketInfo::writeData(const char *msg, const size_t &msgSize)
+{
+    auto retVal = write(sockfd, msg, msgSize);
+    if(retVal < 0)
+    {
+        auto err = errno;
+        throwSystemError(err,"Error writing data to socket.");
+    }
+    else
+        LOG4CPLUS_TRACE(logger, "Wrote "<<retVal<<" bytes");
+
+    return retVal;
+}
+
+} //namespace tlslookieloo
