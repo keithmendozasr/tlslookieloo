0958209b727ac0e18ad3cf69f470bffa04633087
Keith Mendoza <keith@homepluspower.info>
2020-02-01 15:23:25 -0800

    Switch to MOCK_METHOD without param counter.

--- tests/unit/mockwrapper.h
+++ tests/unit/mockwrapper.h
@@ -29,22 +29,23 @@ namespace tlslookieloo
 class MockWrapper : public Wrapper
 {
 public:
-    MOCK_METHOD5(select, int(int nfds, fd_set *readfds, fd_set *writefds,
-        fd_set *exceptfds, struct timeval *timeout));
-    MOCK_METHOD2(SSL_get_error, int(const SSL *, int));
-    MOCK_METHOD3(SSL_read, int(SSL *, void *, int));
-    MOCK_METHOD3(SSL_write, int(SSL *, const void *, int));
-    MOCK_METHOD3(ostream_write, void(std::ostream &, const char * const, const size_t &));
-    MOCK_METHOD4(getaddrinfo, int(const char *, const char *,
-        const struct addrinfo *, struct addrinfo **));
-    MOCK_METHOD3(socket, int(int, int, int));
-    MOCK_METHOD5(setsockopt, int(int, int, int, const void *, socklen_t));
-    MOCK_METHOD3(bind, int(int, const struct sockaddr *, socklen_t));
-    MOCK_METHOD2(listen, int(int, int));
-    MOCK_METHOD3(accept, int(int, struct sockaddr *, socklen_t *));
-    MOCK_METHOD3(fcntl, int(int, int, int));
-    MOCK_METHOD5(getsockopt, int(int, int, int, void *, socklen_t *));
-    MOCK_METHOD3(connect, int(int, const struct sockaddr *, socklen_t));
+    MOCK_METHOD(int, select, (int nfds, fd_set *readfds, fd_set *writefds,
+        fd_set *exceptfds, struct timeval *timeout), (override));
+    MOCK_METHOD(int, SSL_get_error, (const SSL *, int), (override));
+    MOCK_METHOD(int, SSL_read, (SSL *, void *, int), (override));
+    MOCK_METHOD(int, SSL_write, (SSL *, const void *, int), (override));
+    MOCK_METHOD(void, ostream_write,
+        (std::ostream &, const char * const, const size_t &), (override));
+    MOCK_METHOD(int, getaddrinfo, (const char *, const char *,
+        const struct addrinfo *, struct addrinfo **), (override));
+    MOCK_METHOD(int, socket, (int, int, int), (override));
+    MOCK_METHOD(int, setsockopt, (int, int, int, const void *, socklen_t), (override));
+    MOCK_METHOD(int, bind, (int, const struct sockaddr *, socklen_t), (override));
+    MOCK_METHOD(int, listen, (int, int), (override));
+    MOCK_METHOD(int, accept, (int, struct sockaddr *, socklen_t *), (override));
+    MOCK_METHOD(int, fcntl, (int, int, int), (override));
+    MOCK_METHOD(int, getsockopt, (int, int, int, void *, socklen_t *), (override));
+    MOCK_METHOD(int, connect, (int, const struct sockaddr *, socklen_t), (override));
 
     std::unique_ptr<struct addrinfo, decltype(&freeaddrinfo)> defaultAddrInfo;
 
